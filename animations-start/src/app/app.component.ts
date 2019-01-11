cdbca24b4dc4c2d80269f44c0cf4c0d56852f444
dono1986 <donoghue1986@yahoo.de>
2019-01-11 18:30:04 +0100

    Angular Tutorial: Animations abgeschlossen und in Beschreibung ergänzt

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
+    // Animation von Listenelementen beim Hinzufügen und Entfernen
+    trigger('list1', [
+      state('*', style({opacity: 1, transform: 'translateX(0px)'})),
+      transition('void => *', [style({opacity: 0, transform: 'translateX(-100px)'}), animate(400)]),
+      transition('* => void', animate(300, style({opacity: 0, transform: 'translateX(-100px)'})))
+    ]),
+
+    // Animation von Listenelementen beim Hinzufügen und Entfernen (mit Keyframes)
+    trigger('list2', [
+      state('in', style({opacity: 1, transform: 'translateX(0)'})), 
+      transition('void => *', [
+        // Styleveränderungen während der Transition, also in diesem Fall innerhalb einer Sekunde
+        animate(1000, keyframes([
+          style({transform: 'translateX(-100px)', opacity: 0, offset: 0}),
+          style({transform: 'translateX(-50px)', opacity:0.5, offset: 0.3}),
+          style({transform: 'translateX(-20px)', opacity:1, offset: 0.8}),
+          style({opacity: 1, offset: 1, transform: 'translateX(0px)'})
+        ]))
+      ]
+      ),
+      transition('* => void', animate(300, style({opacity: 0, transform: 'translateX(-100px)'})))
+    ]),
+
+    // Animation von Listenelementen beim Hinzufügen und Entfernen mit Grouping
+    trigger('list3', [
+      state('*', style({opacity: 1, transform: 'translateX(0px)'})),
+      transition( 'void => *', 
+                  [ 
+                    style({
+                      opacity: 0, 
+                      transform: 'translateX(-100px)'}), 
+                    animate(400)
+                  ]),
+      transition( '* => void', 
+                  [
+                    // Gruppierung der beiden Animationen, damit diese gleichzeitig ausgeführt werden
+                    group([
+                      animate(  300, 
+                        style({
+                          'color': 'red'}
+                        )
+                      ),
+                      animate(  300, 
+                              style({
+                                opacity: 0, 
+                                transform: 'translateX(-100px)'}
+                              )
+                      )
+                    ])
+                  ])
+    ]),
+
+    // Animation eines Div-Containers mit zwei States
     trigger('divState', [
       state('normal',
             style({'background-color': 'red',
@@ -15,8 +68,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
             transform: 'translateX(100px)'})
             ),
       transition('normal <=> highlighted', animate(300))
-      // transition('highlighted => normal', animate(800)),
     ]),
+
+    // Animation eines Div-Containers mit drei States und Wildcard-Operator und Zwischenschritten
     trigger('wildState', [
       state('normal',
             style({'background-color': 'red',
@@ -24,7 +78,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
             ),
       state('highlighted',
             style({'background-color': 'blue',
-            transform: 'translateX(100px)scale(1)'})
+            transform: 'translateX(100px) scale(1)'})
             ),
       state('shrunken',
             style({'background-color': 'blue',
@@ -33,12 +87,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('highlighted => normal', animate(700)),
       transition('normal => highlighted', animate(400)),
       transition('shrunken <=> *', [
-        style({
+        // Setze Farbe auf Orange, ...
+        animate(1000, style({
           'background-color': 'orange'
-        }),
+        })),
+        // Verändere den Rahmenradius
         animate(1000, style({
           'border-radius': '50px'
         })),
+        // ... und animiere über eine halbe Sekunde bis zum Endstatus
         animate(500)
       ])
     ])
@@ -63,6 +120,16 @@ export class AppComponent {
       this.wildState = (this.wildState === 'normal' ? 'highlighted' : 'normal');
 
     }
+
+    onAnimationStarted(event) {
+      console.log(event);
+    }
+
+
+    onAnimationEnded(event) {
+      console.log(event);
+    }
+
 
     onShrink() {
       this.wildState = (this.wildState === 'normal' ? 'shrunken' : 'normal');
